9fae19d5498a6e6806e24ffa37e6cc7096fe74be
Lodis Perkins <lodisperkins44@gmail.com>
2020-10-12 17:14:09 -0500

    Defined all functions in the scene class. Added input for actor

--- MathForGames/Game.cs
+++ MathForGames/Game.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace MathForGames
 {
     class Game
     {
         private static bool _gameOver = false;
+        private Scene _scene;
 
         //Static function used to set game over without an instance of game.
         public static void SetGameOver(bool value)
@@ -15,24 +17,42 @@ namespace MathForGames
             _gameOver = value;
         }
 
+        //Return wether or not the specified console key is pressed
+        public static bool CheckKey(ConsoleKey key)
+        {
+            
+            if (Console.KeyAvailable)
+            {
+                if (Console.ReadKey(true).Key == key)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         //Called when the game begins. Use this for initialization.
         public void Start()
         {
-
+            _scene = new Scene();
+            Actor actor = new Actor();
+            _scene.AddActor(actor);
         }
+
 
 
         //Called every frame.
         public void Update()
         {
-
+            _scene.Update();
         }
 
         //Used to display objects and other info on the screen.
         public void Draw()
         {
-
+            Console.Clear();
+            _scene.Draw();
         }
 
 
@@ -50,8 +70,11 @@ namespace MathForGames
 
             while(!_gameOver)
             {
+                
                 Update();
                 Draw();
+                
+                Thread.Sleep(50);
             }
 
             End();
