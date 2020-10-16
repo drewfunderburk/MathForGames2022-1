2add388eab9a38b4564401df47ba6c2105281f1d
Lodis Perkins <lodisperkins44@gmail.com>
2020-10-16 13:32:37 -0500

    Added overloads for addition and multiplication;

--- MathForGames/Actor.cs
+++ MathForGames/Actor.cs
@@ -54,8 +54,7 @@ namespace MathForGames
         
         public virtual void Update()
         {
-            _position.X += _velocity.X;
-            _position.Y += _velocity.Y;
+            _position += _velocity;
             _position.X = Math.Clamp(_position.X, 0, Console.WindowWidth-1);
             _position.Y = Math.Clamp(_position.Y, 0, Console.WindowHeight-1);
 
--- MathLibrary/Vector2.cs
+++ MathLibrary/Vector2.cs
@@ -44,5 +44,23 @@ namespace MathLibrary
             _y = y;
         }
 
+        public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
+        {
+            float x = lhs.X + rhs.X;
+            float y = lhs.Y + rhs.Y;
+
+            return new Vector2(x, y);
+        }
+
+        public static Vector2 operator *(Vector2 lhs, float scalar)
+        {
+            return new Vector2(lhs.X * scalar, lhs.Y * scalar);
+        }
+
+        public float GetMagnitude()
+        {
+            return (float)Math.Sqrt(X * X + Y * Y);
+        }
+
     }
 }
